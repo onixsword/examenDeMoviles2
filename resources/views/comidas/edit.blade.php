2fdad5e60e892a7c74a5dc9ec300e83718f04263
Maestro <Maestro@VICTOR.local>
2018-03-21 16:22:44 -0700

    examen completo

---
+++ resources/views/comidas/edit.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comidas</title>
+</head>
+<body>
+    <h1>Editar comida</h1>
+    <a href="{{route('comidas.index')}}">Volver a comidas</a>
+    @if($exito)
+        <p>La comida se actualizó</p>
+    @endif
+    <form method="POST" action="{{ route('comidas.update',array('comidas'=>$comida->id)) }}">
+        {{ csrf_field() }}
+        <input type="hidden" name="_method" value="PUT">
+        <label>Nombre:</label>
+        <input type="text" name="txtNombre" value="{{$comida->nombre}}">
+        <label>Precio:</label>
+        <input type="text" name="txtPrecio" value="{{$comida->precio}}">
+        <label>Descripcion:</label>
+        <input type="text" name="txtDescripcion" value="{{$comida->descripcion}}">
+        <button type="submit">Actualizar comida</button>
+    </form>
+    <form method="POST" action="{{route('comidas.destroy',array('comidas'=>$comida->id))}}">
+        {{ csrf_field() }}
+        <input type="hidden" name="_method" value="DELETE">
+        <button type="submit">Borrar comida</button>
+    </form>
+</body>
+</html>
