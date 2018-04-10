3e77de56b366a5a41195916b69bfdc22469ce18e
Maestro <Maestro@iMac-de-Maestro-10.local>
2018-04-09 18:34:41 -0700

    mas bonito

--- resources/views/comidas/create.blade.php
+++ resources/views/comidas/create.blade.php
@@ -1,22 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comidas</title>
-</head>
+@extends('layouts.default')
+@section('content')
 <body>
     <h1>Nueva comida</h1>
     <form method="POST" action="{{ route('comidas.store') }}">
         {{ csrf_field() }}
-        <label>Nombre:</label>
-        <input type="text" name="txtNombre">
-        <label>Precio:</label>
-        <input type="text" name="txtPrecio">
-        <label>Descripcion:</label>
-        <input type="text" name="txtDescripcion">
+        <div class="form-group">
+            <label class="form-label">Nombre:</label>
+            <input class="form-control" type="text" name="txtNombre">
+        </div>
+        <div class="form-group">
+            <label class="form-label">Precio:</label>
+            <input class="form-control" type="text" name="txtPrecio">
+        </div>
+        <div class="form-group">
+            <label class="form-label">Descripcion:</label>
+            <input class="form-control" type="text" name="txtDescripcion">
+        </div>
         <button type="submit">Agregar comida</button>
     </form>
-</body>
-</html>
+@endsection
--- resources/views/comidas/edit.blade.php
+++ resources/views/comidas/edit.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comidas</title>
-</head>
-<body>
+@extends('layouts.default')
+@section('content')
     <h1>Editar comida</h1>
     <a href="{{route('comidas.index')}}">Volver a comidas</a>
     @if($exito)
@@ -14,13 +7,19 @@
     @endif
     <form method="POST" action="{{ route('comidas.update',array('comidas'=>$comida->id)) }}">
         {{ csrf_field() }}
-        <input type="hidden" name="_method" value="PUT">
-        <label>Nombre:</label>
-        <input type="text" name="txtNombre" value="{{$comida->nombre}}">
-        <label>Precio:</label>
-        <input type="text" name="txtPrecio" value="{{$comida->precio}}">
-        <label>Descripcion:</label>
-        <input type="text" name="txtDescripcion" value="{{$comida->descripcion}}">
+            <input type="hidden" name="_method" value="PUT">
+        <div class="form-group">
+            <label class="form-label">Nombre:</label>
+            <input class="form-control" type="text" name="txtNombre" value="{{$comida->nombre}}">
+        </div>
+        <div class="form-group">
+            <label class="form-label">Precio:</label>
+            <input class="form-control" type="text" name="txtPrecio" value="{{$comida->precio}}">
+        </div>
+        <div class="form-group">
+            <label class="form-label">Descripcion:</label>
+            <input class="form-control" type="text" name="txtDescripcion" value="{{$comida->descripcion}}">
+        </div>
         <button type="submit">Actualizar comida</button>
     </form>
     <form method="POST" action="{{route('comidas.destroy',array('comidas'=>$comida->id))}}">
@@ -28,5 +27,4 @@
         <input type="hidden" name="_method" value="DELETE">
         <button type="submit">Borrar comida</button>
     </form>
-</body>
-</html>
+@endsection
